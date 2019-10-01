d98128c1a485f7b54d2802cefee4e295c71b23ee
monis <monis5362413@gmail.com>
2019-10-01 21:33:28 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_counter_provider/counter_model.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  final counter = CounterModel();
+  final textSize = 48;
+
+  runApp(
+    Provider<int>.value(
+      value: textSize,
+      child: ChangeNotifierProvider<CounterModel>.value(
+        value: counter,
+        child: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: FirstScreen(),
+    );
+  }
+}
